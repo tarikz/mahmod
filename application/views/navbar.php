87bd436d5e5a86836b03ef65868837f6374de8b1
w1252983 <w1252983@my.westminster.ac.uk>
2013-01-08 20:55:17 +0000

    First commit of application directory

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -4,7 +4,7 @@
 
        
       <a href="<?php echo site_url('find/index') ?>">Search</a> &nbsp;  &nbsp;  &nbsp; 
-     <a href="<?php echo site_url('LoginController/index') ?>"> Login </a>  &nbsp;  &nbsp;  &nbsp; 
+     <a href="<?php echo str_replace('http','https', site_url('LoginController/index')) ?>"> Login </a>  &nbsp;  &nbsp;  &nbsp; 
        
         
         </h2>  </center> 
